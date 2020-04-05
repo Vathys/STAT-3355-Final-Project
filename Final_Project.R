264ec57a8c8f76ed0ee903b2c7f7f01739db5c93
Vathys <deepdesai00@gmail.com>
2020-04-04 21:13:18 -0500

    Update Final_Project.R

--- Final_Project.R
+++ Final_Project.R
@@ -1,7 +1,6 @@
 # Questions we need to answer:
 # Did Texas become a more diverse state from 2000-2010? 
 # What is the distribution of race between urban and rural areas?
-# Can we accurately predict the increase in population by the number of children?
 # Is there a correlation between multigenerational living and the number of children in a household?
 # Which race is more likely to live in a multigenerational household?
 
@@ -15,6 +14,7 @@ library(gridExtra)
 census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
 var_10 <- load_variables(year = "2010", dataset = 'sf1', cache = TRUE)
+var_10_sf2 <- load_variables(year = "2010", dataset = 'sf2', cache = TRUE)
 var_00 <- load_variables(year = "2000", dataset = 'sf1', cache = TRUE)
 
 # ANSWERING QUESTION 1
@@ -80,4 +80,44 @@ p_16_10 <- get_decennial(geography = "county", table = "P016",
                          year = 2010, state = "TX")
 p_17_10 <- get_decennial(geography = "county", table = "P017",
                          year = 2010, state = "TX", show_call = TRUE)
-pct_14_10 <- 
+
+var_names <- c("White", "Black/African American", "American Indian/Native Alaskan", "Asian", "Native Hawaiian/Pacific Islander", "Other Race", "Two or More")
+
+multi_sum <- c("PCT014A001", "PCT014B001", "PCT014C001", "PCT014D001", "PCT014E001","PCT014F001", "PCT014G001")
+
+multi_g_002 <- c("PCT014A002", "PCT014B002", "PCT014C002", "PCT014D002", "PCT014E002","PCT014F002", "PCT014G002")
+
+pct_14_002_10 <- get_decennial(geography = "county", variables = multi_g_002[1], 
+                               summary_var = multi_sum[1],
+                               year = 2010, state = "TX", show_call = TRUE) %>%
+  mutate(percent = value / summary_value)
+
+for(i in seq(from = 2, to = length(multi_g_002))){
+  pct_14_002_10 <- pct_14_002_10 %>% bind_rows(get_decennial(geography = "county", variables = multi_g_002[i], 
+                                 summary_var = multi_sum[i],
+                                 year = 2010, state = "TX", show_call = TRUE) %>%
+    mutate(percent = value / summary_value))
+}
+
+three_or_more <- ggplot(pct_14_002_10, mapping = aes(x = variable, y = percent)) + 
+  geom_violin(draw_quantiles = 0.5) + labs(title = "Three or more generations") + ylim (0, 0.5)
+
+multi_g_003 <- c("PCT014A003", "PCT014B003", "PCT014C003", "PCT014D003", "PCT014E003","PCT014F003", "PCT014G003")
+
+pct_14_003_10 <- get_decennial(geography = "county", variables = multi_g_003[1], 
+                               summary_var = multi_sum[1],
+                               year = 2010, state = "TX", show_call = TRUE) %>%
+  mutate(percent = value / summary_value)
+
+for(i in seq(from = 2, to = length(multi_g_003))){
+  pct_14_003_10 <- pct_14_003_10 %>% bind_rows(get_decennial(geography = "county", variables = multi_g_003[i], 
+                                                             summary_var = multi_sum[i],
+                                                             year = 2010, state = "TX", show_call = TRUE) %>%
+                                                 mutate(percent = value / summary_value))
+}
+
+
+two_or_less <- ggplot(pct_14_003_10, mapping = aes(x = variable, y = percent)) + 
+  geom_violin(draw_quantiles = 0.5) + labs(title = "Two or less generations") + ylim(0.5, 1)
+
+grid.arrange(two_or_less, three_or_more, nrow = 2)
